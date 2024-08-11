21df8807c2eed5f61dab414bb00aeb804fa9f624
yabool2001 <mzemlo.pl@gmail.com>
2024-08-11 13:29:13 +0200

    aaaa

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 
 file_path = 'ael/Place-CEMID-Sensor-Sensor1-To-Satellite-POLMEO11_AER.csv'
@@ -6,19 +7,27 @@ file_path = 'ael/Place-CEMID-Sensor-Sensor1-To-Satellite-POLMEO11_AER.csv'
 p = Path('ael')
 for child in p.iterdir () :
     print ( child )
-    data_raw = pd.read_csv ( child , on_bad_lines = 'skip' )
+    data_raw = pd.read_csv ( child , on_bad_lines = 'skip' , delimiter = ',' )
 
-data_polmeo1 = data_raw
-# Convert the 'Elevation (deg)' column to numeric, forcing errors to NaN
-data_polmeo1['Elevation (deg)'] = pd.to_numeric ( data_polmeo1['Elevation (deg)'] , errors='coerce' )
+data = data_raw
+# Convert values, forcing errors to NaN
+data['Time (UTCG)'] = pd.to_datetime ( data['Time (UTCG)'] , errors='coerce' )
+data['Azimuth (deg)'] = pd.to_numeric ( data['Azimuth (deg)'] , errors='coerce' )
+data['Elevation (deg)'] = pd.to_numeric ( data['Elevation (deg)'] , errors='coerce' )
+data['Range (lm)'] = pd.to_numeric ( data['Range (km)'] , errors='coerce' )
 
-# Drop rows where 'Elevation (deg)' is NaN
-data_polmeo1 = data_polmeo1.dropna ( subset = ['Elevation (deg)'] )
+# Drop rows where value is NaN
+data = data.dropna ( subset = ['Time (UTCG)'] )
+data = data.dropna ( subset = ['Azimuth (deg)'] )
+data = data.dropna ( subset = ['Elevation (deg)'] )
+data = data.dropna ( subset = ['Range (km)'] )
 
-max_elevation_polmeo1 = data_polmeo1['Elevation (deg)'].max ()
+max_elevation = data['Elevation (deg)'].max ()
 
 # Find the row number where the maximum elevation value is located
-max_elevation_row_polmeo1 = data_polmeo1[data_polmeo1['Elevation (deg)'] == max_elevation_polmeo1].index[0]
+max_elevation_row_polmeo1 = data[data['Elevation (deg)'] == max_elevation].index[0]
 
-print ( "Maximum Elevation POLMEO1:", max_elevation_polmeo1)
+print ( "Maximum Elevation POLMEO1:", max_elevation )
 print ( "Row Number POLMEO1:", max_elevation_row_polmeo1 )
+
+print ( data['Time (UTCG)'][0] )
